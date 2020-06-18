90f6ea3fb27f1f3fcb88a41415dffaec224383c3
Stephen Sarpong-sei <ssarpong-sei@inlaks.com>
2020-06-18 15:32:10 +0100

    saved requests in attribute variable

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Tweet; // add the tweet model for it to access Tweet::
 use Illuminate\Http\Request;
 
 class TweetController extends Controller
@@ -15,14 +16,27 @@ class TweetController extends Controller
         // or set guarded to an empty array
 
         //validate the request
-        request()->validate(['body' => 'required|max:255']);
+        $attributes = request()->validate(['body' => 'required|max:255']);
 
         Tweet::create([
             'user_id' => auth()->id(),
-            'body' => request('body')
+            'body' => $attributes['body']
         ]);
 
         return redirect('/home');
 
+    }
+
+
+    
+    public function index()
+    {
+        // to the get the latest tweets first
+       // $tweets = Tweet::latest()->get();
+
+        return view('home',[
+            //user() is being called from the Tweet model
+            'tweets' => auth()->user()->timeline()
+        ]);
     }
 }
