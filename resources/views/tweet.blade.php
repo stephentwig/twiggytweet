36020c19b0a8953643287a9ff52328f312961944
Stephen Sarpong-sei <ssarpong-sei@inlaks.com>
2020-06-18 13:12:37 +0100

    added tweet blade

---
+++ resources/views/tweet.blade.php
@@ -0,0 +1,23 @@
+
+                <div class="flex p-4 border-b border-b-gray-400">
+                    <div class="mr-4 flex-shrink-0">
+                            <img 
+                            src="https://i.pravatar.cc/40" 
+                            alt=""
+                            class="rounded-full mr-2"
+                            >
+
+                            
+                    </div> 
+                
+
+                    <div >
+                        <h5 class="font-bold mb-4"> John Doe</h5>
+                        <p class="text-sm">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Magnam totam maiores laborum qui consequuntur laudantium possimus voluptates, nulla nam voluptatum voluptate, repellendus sapiente recusandae accusamus dolorum modi impedit amet veritatis?
+                        </p>
+                    </div>
+            
+                </div>
+            
