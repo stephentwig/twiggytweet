da5cc27593bfe01b61d2f4fccb5fbe8f7338b56c
Stephen Sarpong-sei <ssarpong-sei@inlaks.com>
2020-06-18 14:07:14 +0100

    added tweet parameters to the view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,27 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Tweet;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
+
     public function __construct()
     {
         $this->middleware('auth');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
+
     public function index()
     {
-        return view('home');
+        // to the get the latest tweets first
+       // $tweets = Tweet::latest()->get();
+
+        return view('home',[
+            //user() is being called from the Tweet model
+            'tweets' => auth()->user()->timeline()
+        ]);
     }
 }
