4a296eb4e609d1d325352c14f493751dd760ab00
Stephen Sarpong-sei <ssarpong-sei@inlaks.com>
2020-06-18 14:24:03 +0100

    added guarded array variable for mass assigment

--- app/Tweet.php
+++ app/Tweet.php
@@ -6,7 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tweet extends Model
 {
-    //
+    // since we are mass assigning, must be empty array
+    protected $guarded = [];
+
     public function user()
     {
         return $this->belongsTo(User::class);
