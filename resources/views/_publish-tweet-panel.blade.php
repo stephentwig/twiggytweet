5416c8ccd0ee7102fa170b7dba52586b9f36ce73
Stephen Sarpong-sei <ssarpong-sei@inlaks.com>
2020-06-18 12:36:11 +0100

    added the publish tweet panel

---
+++ resources/views/_publish-tweet-panel.blade.php
@@ -0,0 +1,24 @@
+<div class="border border-blue-400 rounded-lg p-8">
+                <form action="" method="post">
+                    <textarea 
+                        name="" 
+                        id="" 
+                        class="w-full"
+                        placeholder="What's on your mind?"
+                        >
+                        
+                        </textarea>
+
+                        <hr class="py-4">
+                        <footer class="flex justify-between">
+                            <img 
+                                src="https://i.pravatar.cc/40" 
+                                alt=""
+                                class="rounded-full mr-2"
+                                >
+                                <button type="submit" class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white">Tweet-a-roo!</button>
+
+                        </footer>
+
+                </form>
+            </div>
