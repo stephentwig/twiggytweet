d58559bdda97b701be7104f4c1c1e0be370f4163
Stephen Sarpong-sei <ssarpong-sei@inlaks.com>
2020-06-18 14:24:32 +0100

    added a store function

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -7,4 +7,22 @@ use Illuminate\Http\Request;
 class TweetController extends Controller
 {
     //
+
+    public function store()
+    {
+
+        //because we are mass assigning turn off fillable
+        // or set guarded to an empty array
+
+        //validate the request
+        request()->validate(['body' => 'required|max:255']);
+
+        Tweet::create([
+            'user_id' => auth()->id(),
+            'body' => request('body')
+        ]);
+
+        return redirect('/home');
+
+    }
 }
